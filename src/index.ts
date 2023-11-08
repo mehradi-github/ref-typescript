299c67a1093ba4e2b881a03528d59ef9610aa6ae
Mehradi <mehradi.info@gmail.com>
2023-11-08 08:40:05 +0100

    Showing let number in hex,decimal and octal

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log("Hello world!");
+
+let a: number;
+a = 0b0101; //binary
+a = 0xfff; // hex
+a = 0o671; // octal
+console.log(a);
+console.log(a.toString(8));
+console.log(a.toString(16));

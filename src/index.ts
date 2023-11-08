f3b33c90f35db722d4f4ca7bf63a9c8c02a099d8
Mehradi <mehradi.info@gmail.com>
2023-11-08 09:10:42 +0100

    Showing shift,unshift and push in array

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,10 @@ console.log(a.toString(8));
 console.log(a.toString(16));
 a += 1;
 console.log(a);
+
+let b: number[] = [1, 2, 3, 4];
+b.shift();
+b.push(10);
+b.unshift(5);
+console.log(b);
+console.log(b.splice(1, 3));

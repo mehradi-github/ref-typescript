2eecbe000675ea6cc012def77db3ad448182747a
Mehradi <mehradi.info@gmail.com>
2023-11-08 09:24:57 +0100

    Adding Common types

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,24 @@ console.log(a.toString(16));
 a += 1;
 console.log(a);
 
+//array
 let b: number[] = [1, 2, 3, 4];
 b.shift();
 b.push(10);
 b.unshift(5);
 console.log(b);
 console.log(b.splice(1, 3));
+
+// Dictionary
+let c: { [id: string]: number[] } = { a: [1, 2, 3], b: [4, 5, 6] };
+console.log(c);
+
+//Tuple
+let d: [string, boolean, number] = ["a", false, 1];
+console.log(d[1]);
+
+//Set
+let e: Set<unknown> = new Set([1, "v", false, "v"]);
+e.add("cat");
+e.delete(false);
+console.log(e);
